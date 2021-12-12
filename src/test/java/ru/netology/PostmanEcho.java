e2630fa03c8b5660910a16d0f5674b1ea9b5c275
OlgaKP <89009086+OlgaKP@users.noreply.github.com>
2021-12-12 16:57:35 +0400

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEcho {
+    @Test
+    void shouldPostRequest(){
+        // Given - When - Then
+        // Предусловия
+        given()
+            .baseUri("https://postman-echo.com")
+            .body("{ \"phone\": \"8 (800)-000-12-34\" }") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+        .when()
+            .post("/post")
+        // Проверки
+        .then()
+            .statusCode(200)
+            .body("data", equalTo("{ \"phone\": \"8 (800)-000-12-34\" }"))
+        ;
+    }
+}
